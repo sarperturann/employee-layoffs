0bff65d3d14012f3b0d067057d66935a3b414efb
Sarper Turan <sarper.turan01@gmail.com>
2023-02-05 02:49:02 +0300

    Changed file structure, added a server-side for it to be more ordered

--- server/src/main/kotlin/com/study/employeelayoffs/admin/AdminRepository.kt
+++ server/src/main/kotlin/com/study/employeelayoffs/admin/AdminRepository.kt
@@ -1,6 +1,5 @@
 package com.study.employeelayoffs.admin
 
-import com.study.employeelayoffs.admin.Admin
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
--- server/src/main/kotlin/com/study/employeelayoffs/employee/EmployeeRepository.kt
+++ server/src/main/kotlin/com/study/employeelayoffs/employee/EmployeeRepository.kt
@@ -1,6 +1,5 @@
 package com.study.employeelayoffs.employee
 
-import com.study.employeelayoffs.employee.Employee
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
